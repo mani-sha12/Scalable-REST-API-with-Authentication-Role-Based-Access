4fe8b6b4603132ad4203020273067ac0e3ce9fae
mani-sha12 <manisha2502005@gmail.com>
2026-03-26 20:28:14 +0530

    Create App.js

---
+++ backend/frontend/src/services/import React from 'react'; import Register from './pages/Register'; import Login from './pages/Login'; import Dashboard from './pages/Dashboard';  function App() {   return (     <div>       <h1>Backend-Frontend Project</h1>       <Register />       <Login />       <Dashboard />     </src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Backend-Frontend Project</h1>
+      <Register />
+      <Login />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
